6c0e33dc6da7fd6bc01a423c8725da47184fd0bb
JosePirir <josepirirromero@gmail.com>
2022-03-12 08:22:21 -0600

    Agregar clase Alumno

--- Laboratorio2/Entities/Alumno.cs
+++ Laboratorio2/Entities/Alumno.cs
@@ -1,7 +1,25 @@
+using Laboratorio2.Interface;
+
 namespace Laboratorio2.Entities
 {
-    public class Alumno
+    public class Alumno : Persona, IOperaciones
     {
-        
+        public string Carne {get;set;}
+        public string NumeroCreditos{get;set;}
+
+        public bool EliminarAsignatura(string asignatura)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ListarMisDatos(string identificador)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void TomarAsistencia()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
